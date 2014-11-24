7d487b3cf7aa22392316faa97552220dcda204e5
Dan Parker <dparkermit@yahoo.com>
2014-11-24 11:20:54 -0800

    A36507 V0.2.1
    Updated the version file

--- A36507.X/ETM_CAN_FIRMWARE_VERSION.h
+++ A36507.X/ETM_CAN_FIRMWARE_VERSION.h
@@ -2,8 +2,8 @@
 #define __ETM_CAN_FIRMWARE_VERISON_H
 
 #define FIRMWARE_AGILE_REV     0
-#define FIRMWARE_BRANCH        1
-#define FIRMWARE_MINOR_REV     8
+#define FIRMWARE_BRANCH        2
+#define FIRMWARE_MINOR_REV     1
 
 
 
